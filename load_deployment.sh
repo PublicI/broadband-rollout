424856b1976e5c28f337addf3222b8d2b394fb1c
Joe Yerardi <joe.yerardi@gmail.com>
2018-04-20 17:22:47 -0400

    Fixed script to handle problematic lines

--- load_deployment.sh
+++ load_deployment.sh
@@ -4,9 +4,9 @@
 
 
 psql -U postgres -h db.fivetwentyseven.com -f create.sql -d broadband
-psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2016 FROM STDIN WITH CSV HEADER DELIMITER ',' ENCODING 'UTF-8'" < "data/fbd_us_with_satellite_dec2016_v1.csv" broadband
-psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2014 FROM STDIN WITH CSV HEADER DELIMITER ',' ENCODING 'UTF-8'" < "data/fbd_us_with_satellite_dec2014_V2.csv" broadband
-psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2012 FROM STDIN WITH CSV HEADER DELIMITER '|' ENCODING 'UTF-8'" < "data/December-2012-Random_pt.txt" broadband # Import census blocks larger than two square miles
+psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2016 FROM STDIN WITH CSV HEADER DELIMITER ',' ENCODING 'LATIN1'" < "data/fbd_us_with_satellite_dec2016_v1.csv" broadband
+psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2014 FROM STDIN WITH CSV HEADER DELIMITER ',' ENCODING 'LATIN1'" < "data/fbd_us_with_satellite_dec2014_V2.csv" broadband
+psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2012 FROM STDIN WITH CSV HEADER DELIMITER '|' ENCODING 'LATIN1'" < "data/december_2012_random_pt_fixed.txt" broadband # Import census blocks larger than two square miles
 psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2012 (
 	objectid,
 	frn,
@@ -26,4 +26,4 @@ psql -U postgres -h db.fivetwentyseven.com -c "COPY deployment_2012 (
 	provider_type,
 	end_user_cat
 	)
-	FROM STDIN WITH CSV HEADER DELIMITER '|' ENCODING 'UTF-8'" < "data/December-2012-Census_Block.txt" broadband # Import census blocks smaller than two square miles
+	FROM STDIN WITH CSV HEADER DELIMITER '|' ENCODING 'LATIN1'" < "data/december_2012_census_block_fixed_2.txt" broadband # Import census blocks smaller than two square miles
